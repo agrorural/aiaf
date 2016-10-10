7056cd6e6bb19eb35e6711b58625f7c01a31bcb5
Marcial Soto <marcialsotosanchez@gmail.com>
2016-10-10 15:54:47 -0500

    Analytics included

--- wp-content/themes/aiaf/lib/extras.php
+++ wp-content/themes/aiaf/lib/extras.php
@@ -43,3 +43,18 @@ function aiaf_theme_setup() {
 }
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\aiaf_theme_setup' );
+
+
+add_action('wp_footer', __NAMESPACE__ . '\\analyticstracking');
+function analyticstracking() { ?>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85532203-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<?php } ?>
